377cf5ceeed24280e61931b839d781ef11ed780c
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-10-28 08:29:46 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,7 @@ import { faker } from "@faker-js/faker";
 export default function Page() {
   const number = faker.number.int(100);
 
+  console.log("fake name page rendered");
+
   return <div>Hello World {number}</div>;
 }
