74a34d58e328b3d39d75215147a3d4291d6bccab
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-10-30 00:23:29 +0900

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <pre>{JSON.stringify(data, null, 2)}</pre>
       </body>
     </html>
   );
